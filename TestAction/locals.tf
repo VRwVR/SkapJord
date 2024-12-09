41a51f724a5a58bd497261468178a9b85916a9eb
VRwVR <88397043+VRwVR@users.noreply.github.com>
2024-12-09 21:33:42 +0100

    Delete TestAction directory

--- TestAction/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  workspace_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
-  web_suffix       = "<h1>${terraform.workspace}</h1>"
-
-  rg_name = "${var.rg_name}-${local.workspace_suffix}"
-  sa_name = "${var.sa_name}${local.workspace_suffix}"
-}
--- TestAction/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "primary_web_endpoint" {
-  value = azurerm_storage_account.sa_web.primary_web_endpoint
-}
