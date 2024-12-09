b1a66b7712dbb1bfe89c6364275c4c6989bd83b9
VRwVR <88397043+VRwVR@users.noreply.github.com>
2024-12-09 21:33:31 +0100

    Delete nytestx directory

--- nytestx/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-be-clm-x"
-    storage_account_name = "sabeclmr3d39t1d"
-    container_name       = "scbeclm"
-    key                  = "actiontest2" # NB! Endre for hver konfig!
-  }
-}
